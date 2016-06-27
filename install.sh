f13f8b368d47d2568b71c2b8c7cdf8dc1a28e85d
Leo Allen <leo.f.allen@gmail.com>
2016-06-27 15:09:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,13 @@
 # You may want to setup your computer using Thoughtbot's laptop
 # https://github.com/thoughtbot/laptop
 #
-# curl --remote-name https://raw.githubusercontent.com/thoughtbot/laptop/master/mac | sh
+
+echo "Installing Thoughtbot's laptop setup"
+# curl --remote-name https://raw.githubusercontent.com/thoughtbot/laptop/master/mac && sh mac
 echo "Installing homefiles"
 gem install homesick
 homesick clone https://github.com/pitchinvasion/home-files.git
 cd home-files
 homesick symlink home-files
 homesick rc home-files
-
-echo "Running local install scripts"
-sh $HOME/.laptop.local
-
 echo "Done!"
